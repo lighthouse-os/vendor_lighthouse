faa6b8f8dce9c8e5f44af65e23f961af98a9b075
harsh-23 <harsh.v23labs@gmail.com>
2019-09-16 10:41:11 +0200

    ColorBucket: add a few new dark theme shades [3/3]
    - color hex's from SwiftInstaller app default backgrounds
    
    Change-Id: I98283c4621f89f3a3f2ba7ee48a6815e6e5b2d97
    Signed-off-by: harsh-23 <harsh.v23labs@gmail.com>
    Signed-off-by: Akito Mizukito <akito@evolution-x.org>

--- config/common.mk
+++ config/common.mk
@@ -216,11 +216,22 @@ TARGET_FACE_UNLOCK_SUPPORTED ?= true
 ifeq ($(TARGET_FACE_UNLOCK_SUPPORTED),true)
 PRODUCT_PACKAGES += \
     FaceUnlockService
+
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     ro.face_unlock_service.enabled=$(TARGET_FACE_UNLOCK_SUPPORTED)
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.biometrics.face.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/android.hardware.biometrics.face.xml
 endif
+
+# Settings
+PRODUCT_PACKAGES += \
+    SystemPitchBlackOverlay \
+    SystemDarkGrayOverlay \
+    SystemUIDarkGrayOverlay \
+    SystemStyleOverlay \
+    SystemUIStyleOverlay \
+    SystemNightOverlay \
+    SystemUINightOverlay
 
 # Root
 PRODUCT_PACKAGES += \
