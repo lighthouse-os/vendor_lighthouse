95e43a82192a6ead87af51b6aa82c4756a997961
CannedShroud <hypersync112@gmail.com>
2021-03-12 06:38:01 +0000

    vendor: Gestural navigation by default

--- config/common.mk
+++ config/common.mk
@@ -96,6 +96,10 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.biometrics.face.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/android.hardware.biometrics.face.xml
 endif
 
+# Use gestures by default
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.boot.vendor.overlay.theme=com.android.internal.systemui.navbar.gestural
+
 # Bootanimation
 include vendor/lighthouse/config/bootanimation.mk
 
