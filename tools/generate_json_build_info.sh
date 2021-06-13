120b5e8f116640b4ab8b3c29ec58dfb00fc57cc7
Stealth1226 <rishawniyer06@gmail.com>
2021-06-13 04:13:32 +0000

    Adapt script for lighthouse

--- tools/generate_json_build_info.sh
+++ tools/generate_json_build_info.sh
@@ -9,6 +9,20 @@ then
     md5=$(cat "$file_path.md5sum" | cut -d' ' -f1)
     datetime=$(grep ro\.build\.date\.utc ./out/target/product/$DEVICE/system/build.prop | cut -d= -f2);
     id=$(sha256sum $file_path | awk '{ print $1 }');
-    echo -e "{\n   \"datetime\": $datetime,\n   \"filehash\": \"$md5\",\n   \"filename\": \"$file_name\",\n   \"id\": \"$id\",\n   \"size\": $file_size\n}" > $file_path.json
+    version=$(echo $file_name | cut -d '-' -f2)
+    download="https://sourceforge.net/projects/project-lighthouse/files/$DEVICE/$file_name/download"
+    echo '{
+
+        "error": false,
+        "device": "'$DEVICE'",
+        "id": "'$id'",
+        "filename": "'$file_name'",
+        "url": "'$download'",
+        "datetime": "'$datetime'",
+        "filehash": "'$md5'",
+        "size": '$file_size',
+        "version": "'$version'"
+
+}' > "${file_name}.json"
   fi
 fi
