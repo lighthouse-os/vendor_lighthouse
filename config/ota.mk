8478fbd87f5b5c4b3fe96b8af612c72c37ff94d2
CannedShroud <hypersync112@gmail.com>
2021-10-15 12:16:04 +0530

    vendor: PRODUCT_GENERIC_PROPERTIES -> PRODUCT_SYSTEM_DEFAULT_PROPERTIES
    Signed-off-by: CannedShroud <hypersync112@gmail.com>

--- config/ota.mk
+++ config/ota.mk
@@ -2,7 +2,7 @@ ifeq ($(LIGHTHOUSE_BUILD_TYPE), OFFICIAL)
 
 LIGHTHOUSE_OTA_VERSION_CODE := raft
 
-PRODUCT_GENERIC_PROPERTIES += \
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     ro.lighthouse.ota.version_code=$(LIGHTHOUSE_OTA_VERSION_CODE) \
     sys.ota.disable_uncrypt=1
 
