a15165c6255008dd41d896f8d3b47988cd02d91e
Rishawn <70256146+Stealth1226@users.noreply.github.com>
2022-01-10 16:24:44 +0530

    vendorsetup: override kernel build host info as well

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,3 +1,5 @@
 # Override host metadata to make builds more reproducible and avoid leaking info
 export BUILD_HOSTNAME=lighthouse-build
 export BUILD_USERNAME=lighthouse
+export KBUILD_BUILD_USER=lighthouse
+export KBUILD_BUILD_HOST=lighthouse-build
